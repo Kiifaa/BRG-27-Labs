48833e2c9373d779499b4bd1f7f6b8117b300734
Kiifaa <kiefera20@gmail.com>
2026-04-07 02:25:04 +0800

    Update testscript.sh

--- Scripts/testscript.sh
+++ Scripts/testscript.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-# Get current date for timestamped backup
-now=$(date +"%d_%m_%y")
+now=$(date +"%d_%m_%y_%H%M%S")
 
-# Create backup directory if it doesn't exist
 mkdir -p /home/kiefer/backup
-
-# Copy all files from Documents into backup folder
 cp -R /home/kiefer/Documents/* /home/kiefer/backup/
 
-# Zip the backup folder with date as filename
 zip -r /home/kiefer/$now.zip /home/kiefer/backup/*
 
-# Move the zip file to home directory
-cp /home/kiefer/$now.zip /home/kiefer/
+scp /home/kiefer/$now.zip kiefer@localhost:/home/kiefer/
 
-# Optional: SCP to cloud server (skip if using VirtualBox only)
-# scp -i /home/kiefer/key.pem /home/kiefer/$now.zip ubuntu@<your-server-ip>:/home/ubuntu/
-
-# Print message to confirm script ran
 echo "Backup completed: /home/kiefer/$now.zip"
